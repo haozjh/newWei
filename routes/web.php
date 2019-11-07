68ca22cde51b8f8c4d8570f191d701a45cd87584
zjh <690805319@qq.com>
2019-11-07 17:04:30 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//文章列表页
+Route::get('/posts', 'PostController@index');
+
+//创建文章
+Route::get('/posts/create', 'PostController@create');
+Route::post('/posts', 'PostController@store');
+//文章详情页
+Route::get('/posts/{post}', 'PostController@show');
+//编辑文章
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
+//删除文章
+Route::get('/posts/{post}/delete', 'PostController@delete');
+//图片上传
+Route::post('/posts/image/upload', 'PostController@imageUpload');
+
+
+
