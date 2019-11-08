eada4d2accb96b821bd8d44a7c300e8a6dae2dd4
zjh <690805319@qq.com>
2019-11-08 15:46:42 +0800

    add new

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+//用户模块
+//注册页面
+Route::get('/register', 'RegisterController@index');
+//注册行为
+Route::post('/register', 'RegisterController@register');
+//登录页面
+Route::get('/login', 'LoginController@index');
+//登录行为
+Route::post('/login', 'LoginController@login');
+//登出行为
+Route::get('/logout', 'LoginController@logout');
+//个人设置页面
+Route::get('user/me/setting', 'UserController@setting');
+//个人设置操作
+Route::post('user/me/setting', 'UserController@settingStore');
+
+
 //文章列表页
 Route::get('/posts', 'PostController@index');
-
 //创建文章
 Route::get('/posts/create', 'PostController@create');
 Route::post('/posts', 'PostController@store');
